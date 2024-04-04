f98afce456c19b182eb9c6f8d8e9dc5535da80c2
IvanTorzoni <torzoni.ivan00@gmail.com>
2024-04-04 17:45:40 +0200

    feat: added comparison with array elements and output messages

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -9,6 +9,9 @@ console.log(userRequest);
 // Creo le mie variabili di appoggio
 const word = ""
 const wordReverse = ""
+// Creo una variabile flag per il confronto degli elementi delle parole
+let result = false
+
 
 // Creo gli array. All'interno vado ad inserire le singole lettere della parola fornita dall'utente
 const wordArray = []
@@ -38,3 +41,23 @@ for (let i = userRequest.length - 1; i >= 0; i--) {
     console.log(wordReverseArray);
 }
 
+// Creo il ciclo for per confrontare le lettere presenti nei due array
+for (let i = 0; i < wordArray.length; i++) {
+    // Se gli elementi dell'array word sono diversi da quelli dell'array reverse
+    if (wordArray[i] === wordReverseArray[i]) {
+        // porta la variabile flag da false a true
+        result = true
+    }
+}
+console.log(result);
+
+// Messaggio di output
+// se la variabile result è vera
+if (result === true) {
+    //genera il messaggio seguente
+    alert("La parola da te inserita è palindroma");
+    // altrimenti
+} else {
+    //genera il messaggio seguente
+    alert("la parola da te inserita non è palindroma")
+}
