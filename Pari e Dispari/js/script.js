a9e1ccd64d569a349164083e2be368ddb19342f8
IvanTorzoni <torzoni.ivan00@gmail.com>
2024-04-05 00:18:40 +0200

    feat: requests to user

--- Pari e Dispari/js/script.js
+++ Pari e Dispari/js/script.js
@@ -1,1 +1,13 @@
-alert("ciao")
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
+
+// Richiesta all' utente di scegliere pari o dispari
+const userOdds = prompt("dimmi pari o dispari");
+console.log(`La tua scelta tra pari e dispari è:`, userOdds);
+
+// Richiesta all' utente di scegliere un numero compreso te 1 e 5
+const userNum = parseInt(prompt("dimmi un numero da 1 a 5"));
+console.log(`La scelta del tuo numero è:`, userNum);
