f0eac86bfcfd3c109f15f4802a600133354beea1
IvanTorzoni <torzoni.ivan00@gmail.com>
2024-04-04 17:26:04 +0200

    feat: added request to user via prompt

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -1,1 +1,9 @@
-alert("ciao")
+
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma.
+
+// Chiedo all' utente di inserire una parola
+const userRequest = prompt("Inserisci una parola e ti dirò se è palindroma o meno")
+console.log(userRequest);
+
+
