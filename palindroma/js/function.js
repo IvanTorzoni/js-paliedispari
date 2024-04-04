6546c0e23b6e0b43ba1f79a5807e565d32e57720
IvanTorzoni <torzoni.ivan00@gmail.com>
2024-04-04 22:26:07 +0200

    feat: added JSDoc for the  function

--- palindroma/js/function.js
+++ palindroma/js/function.js
@@ -1,3 +1,10 @@
+/**
+ * 
+ * @param {Array} wordArrayFunc 
+ * @param {Array} arrayReverseFunc 
+ * @returns {boolean}
+ */
+
 function palindrome(wordArrayFunc, arrayReverseFunc) {
     if (wordArrayFunc!== arrayReverseFunc) {
         result = true
