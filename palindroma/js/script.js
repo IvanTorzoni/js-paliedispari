100e7a84976f5a9ed2cf5df11246b99f908e8227
IvanTorzoni <torzoni.ivan00@gmail.com>
2024-04-04 17:27:28 +0200

    feat: added cycle for word elements

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -6,4 +6,21 @@
 const userRequest = prompt("Inserisci una parola e ti dirò se è palindroma o meno")
 console.log(userRequest);
 
+// Creo le mie variabili di appoggio
+const word = ""
+
+// Creo gli array. All'interno vado ad inserire le singole lettere della parola fornita dall'utente
+const wordArray = []
+
+// creo un ciclo for per prendere le singole lettere della parola
+for (let i = 0; i < userRequest.length; i++) {
+    // Associo le singole lettere alla variabile di appoggio
+    let word = userRequest[i];
+    //log alla console
+    console.log(word);
+    //pusho le lettete nell'array
+    wordArray.push(word);
+    //log alla console
+    console.log(wordArray);
+}
 
